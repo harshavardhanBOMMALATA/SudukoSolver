986b9538fc47338ebd350a96b319d116936a30e9
harshavardhanBOMMALATA <hbommalata@gmail.com>
2026-02-08 13:54:25 +0530

    improving settings for render

--- sudukosolver/settings.py
+++ sudukosolver/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=q_9y6jwsv+am20m#t&8ye&nmy8mh78et(t!^grlylrt(0paul'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
